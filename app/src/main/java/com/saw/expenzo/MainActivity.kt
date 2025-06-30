1973d0907876ad23df66f9f7dccdac944c69ba80
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2025-06-30 18:21:26 +0600

    [chore] Cleanup

--- app/src/main/java/com/saw/expenzo/MainActivity.kt
+++ app/src/main/java/com/saw/expenzo/MainActivity.kt
@@ -5,17 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.saw.expenzo.ui.theme.ExpenzoTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,21 +28,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ExpenzoTheme {
-        GreetingText(
-            message = "Expenzo"
-        )
     }
 }
