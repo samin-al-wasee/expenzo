c9b4bcbd1d8f4d4ddaddaebe44ce3f9bcf03217a
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2025-07-11 17:18:23 +0600

    Revert "[Feature] Home Screen Content"
    This reverts commit 83b8116073cef803e6e59e1ba015ca3d1a703b51.

--- app/src/main/java/com/saw/expenzo/Utils.kt
+++
@@ -1,5 +0,0 @@
-package com.saw.expenzo
-
-fun toCamelCase(text: String) = text.split(Regex("[\\s_-]+"))
-    .mapIndexed { index, word -> if (index == 0) word.lowercase() else word.replaceFirstChar { it.uppercaseChar() } }
-    .joinToString("")
